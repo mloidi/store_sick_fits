f1427a88662fa4da32abe36919df6624bf2e4ae6
Mikel Loidi <mikel.loidi@outlook.com>
2018-10-12 15:31:22 +0200

    set up server side GraphQL

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,23 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-module.exports = mutations;
+    return item;
+  }
+
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name};
+  //   global.dogs.push(newDog);
+  //   console.log(args);
+  // }
+};
+
+module.exports = Mutations;
