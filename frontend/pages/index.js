b2cd14d5510e3e7052f002dbfd0d49ec0aa930ed
Mikel Loidi <mikel.loidi@outlook.com>
2018-10-12 12:59:22 +0200

    styled components

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
